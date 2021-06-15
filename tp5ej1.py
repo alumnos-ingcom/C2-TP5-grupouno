26aa16856535a8410440ef5cfa3cb75b54cb44f1
Sebastia MARTIN DARRICADES <sebasamd@gmail.com>
2021-06-15 18:47:09 -0300

    Creo el primer archivo, deben colocar su nombre y usuario @AndresBochi @checas

---
+++ tp5ej1.py
@@ -0,0 +1,18 @@
+################
+# MARTIN DARRICADES Sebastián - @sebasamd
+# *Agregar datos
+# *Agregar datos
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+# Reemplazar por las funciones del ejercicio
+
+
+def prueba():
+    """Toda la interacción con el usuario va acá"""
+    pass
+
+if __name__ == "__main__":
+    prueba()
+
