0092281a9fa20605ce5efe6961b8c84f27603147
ANDRES BOCCHIGLIERE <elmaildeandresbochi@gmail.com>
2021-06-21 16:17:36 -0300

    Agregado ejercicio 14

--- tp5ej14.py
+++ tp5ej14.py
@@ -5,61 +5,31 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
-# Implementar una funcion que retorne una lista con todos los factoriones menores a 1.499.999
+# Escribir una función que determine si un numero entero positivo es capicúa.
 
-   
-def encontrar_factoriales(numero):
+def es_capicua(numero):
     """
-    Esta función devuelve el factorial de un número del 0 al 9 solicitado en el argumento.
-    
-    El factorial de un número entero positivo se define como el producto de todos
-    los números naturales anteriores o iguales a él.
+    Determina si un número ingresado como argumento es capicúa. Funciona para números negativos!
     
     """
+    absoluto = abs(int(numero)) 
+    string_num = str(absoluto)
+    inverso_num = string_num[::-1]
     
-    lista_factoriales = [1,]
-    resultado = 1
-    
-    for i in range (9):
-        resultado = resultado * (i + 1)
-        lista_factoriales.append(resultado)
-    
-    # print(lista_factoriales)
-    
-    return lista_factoriales[numero]
-
-
-def encontrar_factoriones():
-    """
-    Devuelve una lista con con todos los factoriones menores a 1.499.999
-    
-    """
-    # Si quieren una prueba de escritorio de la función, des-comenten los prints de ésta función. Pero bajen el número de
-    # el lazo while para que no se haga eterno!!!
-    
-    busqueda = 0
-    lista_factoriones = []
-    
-    while busqueda < 1500000:
-                # print()
-                resultado_total = 0
-                for caracter in str(busqueda):
-                    resultado_parcial = encontrar_factoriales(int(caracter))
-                    # print(f"El factorial de {caracter} es {resultado_parcial}")
-                    resultado_total += resultado_parcial
-                    
-                # print(f"El resultado total de {busqueda} es {resultado_total}")
-                if busqueda == resultado_total:
-                    lista_factoriones.append(busqueda)
-                busqueda += 1
-
-    return lista_factoriones
-
-
+    if string_num == inverso_num:
+        return True
+    else:
+        return False
+  
 def prueba():
-    
-    print(encontrar_factoriones())  
+    print("Ingrese un número")
+    numero = input()
+    print(f"¿Es {numero} capicúa?")
+    print(es_capicua(numero))
     
 if __name__ == "__main__":
-    prueba()
+        prueba()
+
+
+ 
 
