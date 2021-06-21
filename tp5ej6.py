6ae97a88f36feb008c39e5e3f8cc26aa56d4d540
Sebastia MARTIN DARRICADES <sebasamd@gmail.com>
2021-06-21 01:30:13 -0300

    Entrega Ej6 @AndresBochi @checas @martinvilu

--- tp5ej6.py
+++ tp5ej6.py
@@ -9,7 +9,7 @@
 
 #Implementar una función que determine si una cadena con parentesis está balanceada.
 
-def limpiar_cadena(cadena):
+def filtrar_cadena(cadena):
     """Esta funcion deja solo los caracteres "{[()]}" de una cadena."""
     i = 0
     validos = ("{[()]}")
@@ -22,21 +22,14 @@ def limpiar_cadena(cadena):
                 filtrada.append(n)
         return filtrada
 
-#def transformar_tupla(lista):
-#    largo = len(lista)
-#    i = 0
-#    tupla = str()
-#    while i < largo:
-#        tupla = tupla + str(lista[i]) + ", "
-#        i += 1
-#    return tupla
-
 def balance_cadena(cadena):
-    """Esta funcion determina si una cadena con parentesis está balanceada"""
+    """Esta funcion determina si una cadena se encuentra balanceada, devuelve valor 0 si no y 1 si lo esta."""
     i = 0
-    while i < len(cadena):
+    largo = len(cadena) 
+    if largo == 0:
+        return 0
+    while i < largo:
         if cadena[i] in "{[(":
-            print("1er paso")
             i += 1
         else:
             if cadena[i - 1] + cadena[i] in "{}" or \
@@ -45,28 +38,15 @@ def balance_cadena(cadena):
                     cadena = cadena[:i - 1] + cadena[i + 1:]
                     i -= 1
                     return 1
-            else:
-                return 0
 
 def prueba():
     print("Probando las funciones(TP5EJ6)")
 
     print("Ingrese una cadena para verificar si se encuentra balanceada: \n")
     cadena = input("Ingrese su cadena: ")
-    filtrada = limpiar_cadena(cadena)
-    print(type(filtrada))
-    print(filtrada)
-    
-#    tupla = transformar_tupla(filtrada)
-#    print(f"\n tupla {tupla}")
-#    print(type(tupla))
-
-#    verificar = balance_cadena(tupla)
-#    print(verificar)
-    
-    print("\n string")
-    verificar2 = balance_cadena(filtrada)
-    print(verificar2)
+    filtrada = filtrar_cadena(cadena)
+    verificar = balance_cadena(filtrada)
+    print(verificar)
     
 if __name__ == "__main__":
     prueba()
