b300e1d6b1ced418d39691060c942891de22ad77
ANDRES BOCCHIGLIERE <elmaildeandresbochi@gmail.com>
2021-06-21 14:52:15 -0300

    Agregado comentario en ej 9

--- tp5ej9.py
+++ tp5ej9.py
@@ -16,7 +16,7 @@ def encontrar_factoriales(numero):
     
     """
     
-    lista_factoriales = [1,]
+    lista_factoriales = [1,] # El 0 por definición tiene factorial = 1, por eso inicializamos la lista con ese valor.
     resultado = 1
     
     for i in range (9):
