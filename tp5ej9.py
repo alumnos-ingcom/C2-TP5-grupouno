7709f3e903880cae141463473cc84d91fa7d226c
Sebastia MARTIN DARRICADES <sebasamd@gmail.com>
2021-06-21 11:43:14 -0300

    intento el Ej9

--- tp5ej9.py
+++ tp5ej9.py
@@ -9,11 +9,24 @@
 
 def factorial(numero):
     """Factorial"""
-    producto = numero
+    producto = int(numero)
     for i in range(2, numero):
         producto *= i
         print(producto)
     return producto
+
+def separar(numero):
+    lista = []
+    i = 0
+    for i in str(numero):
+        lista.append(i)
+    return lista
+    
+def factorizar(lista):
+    for i in lista:
+        factorizar = factorial(i)
+        print(factorizar)
+
 
 def prueba():
     print("Probando las funciones(TP5EJ6)")
@@ -21,8 +34,15 @@ def prueba():
     print("Ingrese una cadena para verificar si se encuentra balanceada: \n")
     cadena = input("Ingrese su cadena: ")
     entero = int(cadena)
-    verificar = factorial(entero)
-    print(verificar)
+    separado = separar(entero)
+    fact = factorizar(separado)
+    print(factorizar)
+    print(type(separado))
+    print(separado)
+    #verificar = factorial(entero)
+    #print(verificar)
+    
+    
     
 if __name__ == "__main__":
     prueba()
