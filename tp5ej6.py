6b57e935f4993a5ca5a92694e089a8296a7ac951
Sebastia MARTIN DARRICADES <sebasamd@gmail.com>
2021-06-20 23:24:02 -0300

    Primer subida Ej6

---
+++ tp5ej6.py
@@ -0,0 +1,22 @@
+################
+# MARTIN DARRICADES Sebastián - @sebasamd sebasamd@gmail.com
+# BOCCHIGLIERE Andrés - @AndresBochi - elmaildeandresbochi@gmail.com
+# Sergio Checcarelli - @checas
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+# Reemplazar por las funciones del ejercicio
+
+#Implementar una función que determine si una cadena con parentesis está balanceada.
+
+
+def balance_cadena(cadena):
+        
+
+def prueba():
+    
+    
+
+if __name__ == "__main__":
+    prueba()
+
