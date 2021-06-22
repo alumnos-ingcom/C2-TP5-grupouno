85bb881374ca0316d9c6e1b183332ae61d483f23
Sebastia MARTIN DARRICADES <sebasamd@gmail.com>
2021-06-22 09:32:11 -0300

    intento ej11

---
+++ tp5ej11.py
@@ -0,0 +1,18 @@
+################
+# MARTIN DARRICADES Sebastián - @sebasamd
+# BOCCHIGLIERE Andrés - @AndresBochi - elmaildeandresbochi@gmail.com
+# CHECCARELLI Sergio -@checas
+# *Agregar datos
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+#Un promedio móvil es una media no ponderada de una cantidad menor al total de un conjunto de valores en una serie de datos. Es una forma de controlar variaciones fuertes en la misma. Esta técnica permite ajustar el nivel de suavizado cambiando la cantidad de valores contiguos de la serie que entran en el calculo.
+
+def promedio_movil():
+
+def prueba():
+    
+
+
+if __name__ == "__main__":
+    prueba()
